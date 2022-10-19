e7adff2b4f489da52f077d2b011d9ee06bda49c0
BZoennchen <zoennchen.benedikt@hm.edu>
2022-10-19 08:50:15 +0200

    update seconds script.

--- solutions/training00/seconds.py
+++ solutions/training00/seconds.py
@@ -1,4 +1,4 @@
-print('Geben Sie die Anzahl an Tagen an [Enter]:')
+print('Geben Sie die Anzahl an Tagen an und bestätigen Sie mit [Enter]:')
 days = int(input())
 seconds = days * 24 * 60 * 60
 print(f'In {days} Tagen verstreichen {seconds} Sekunden.')
